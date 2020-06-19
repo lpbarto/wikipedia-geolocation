e33192b195a652cf3af79eaa6b0768a49e359736
lapo <lapo.bartolacci97@gmail.com>
2020-06-19 17:35:09 +0200

    config-added

---
+++ server/config.php
@@ -0,0 +1,13 @@
+<?php
+$db_host = 'localhost';
+$db_user = 'wikimaps';
+$db_pass = 'WikiMaps2020';
+$db_name = 'wikimaps_db';
+$db_connect = mysql_connect($db_host,$db_user , $db_pass);
+if (!$db_connect) {
+die('Not connected : ' . mysql_error());
+}
+if (! mysql_select_db($db_name) ) {
+die ('Can\'t use foo : ' . mysql_error());
+}
+?>

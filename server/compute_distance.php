f1b3621d66222d8e8f738e64041440435f16ad9d
lapo <lapo.bartolacci97@gmail.com>
2020-06-22 22:06:04 +0200

    find pages near location

---
+++ server/compute_distance.php
@@ -0,0 +1,21 @@
+<?php
+
+function distance($lat1, $lon1, $lat2, $lon2) {
+  if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+    return 0;
+  }
+  else {
+    $theta = $lon1 - $lon2;
+    $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+    $dist = acos($dist);
+    $dist = rad2deg($dist);
+    $miles = $dist * 60 * 1.1515;
+
+    return ($miles * 1.609344);
+    
+    }
+}
+
+echo distance(43.771388888, 11.254166666, 43.778138, 11.259002);
+
+?>
